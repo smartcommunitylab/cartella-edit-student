6012ea799f0860091f450fd03603f4b525adc371
Nawaz Khurshid <nawaz1981@gmail.com>
2020-08-11 16:49:07 +0200

    Fix for asl2-218
    component structure, top information bar, scroll to specific item

--- src/app/tab2/tab2-routing.module.ts
+++ src/app/tab2/tab2-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Tab2Page } from './tab2.page';
 import { GestionePresenzeGruppoPage } from './actions/gestione-presenze-gruppo/gestione-presenze-gruppo.page'
+import { GestionePresenzeIndividualePage } from './actions/gestione-presenze-individuale/gestione-presenze-individuale.page';
 
 const routes: Routes = [
   { path: '', component: Tab2Page },
   { path: 'presenze/gruppo', component: GestionePresenzeGruppoPage },
+  { path: 'presenze/individuale', component: GestionePresenzeIndividualePage },
+ 
 ];
 
 @NgModule({
@@ -16,5 +19,6 @@ export class Tab2PageRoutingModule {
   static components = [
     Tab2Page,
     GestionePresenzeGruppoPage,
+    GestionePresenzeIndividualePage,
   ];
 }
