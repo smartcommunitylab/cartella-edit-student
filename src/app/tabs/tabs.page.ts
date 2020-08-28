dd629545b9741b836b9d63f90e1993ab9a8b0430
Nawaz Khurshid <nawaz1981@gmail.com>
2020-08-28 16:57:24 +0200

    Update tabs.page.ts

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -11,15 +11,11 @@ export class TabsPage {
 
   constructor(private keyboard: Keyboard) { 
 
-    this.keyboard.onKeyboardShow().subscribe(() => {
-      // This is never executed...
-      console.log('Keyboard is now open');
-    });
-    
-    this.keyboard.onKeyboardHide().subscribe(() => {
-      // This is never executed...
-      console.log('Keyboard is now close');
-  });
+    window.addEventListener('keyboardWillShow', (e) => { console.log('keyboardWillShow');}); 
+    window.addEventListener('keyboardWillHide', () => { console.log('keyboardWillHide'); });
+    window.addEventListener('keyboardDidShow', (e) => {console.log('keyboardDidShow');}); 
+    window.addEventListener('keyboardDidHide', () => {console.log('keyboardDidHide');});
+ 
 
   }
 
