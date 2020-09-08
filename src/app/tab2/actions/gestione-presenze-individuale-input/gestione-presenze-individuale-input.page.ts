f0c2a046d971173690b2fe394d3440ae122b6f62
Nawaz Khurshid <nawaz1981@gmail.com>
2020-09-08 13:17:24 +0200

    Fix for asl2-247

--- src/app/tab2/actions/gestione-presenze-individuale-input/gestione-presenze-individuale-input.page.ts
+++ src/app/tab2/actions/gestione-presenze-individuale-input/gestione-presenze-individuale-input.page.ts
@@ -23,6 +23,7 @@ export class GestionePresenzeIndividualeInputPage {
   percentage;
   ore = [ { text: 'Assente', value: '0' }, { text: '1', value: '1' }, { text: '2', value: '2' }, { text: '3', value: '3' }, { text: '4', value: '4' }, { text: '5', value: '5' }, { text: '6', value: '6' }, { text: '7', value: '7' }, { text: '8', value: '8' }, { text: '9', value: '9' }, { text: '10', value: '10' }, { text: '11', value: '11' }, { text: '12', value: '12' }]
   env = environment;
+  picker;
 
   constructor(
     private dataService: DataService,
@@ -130,10 +131,16 @@ export class GestionePresenzeIndividualeInputPage {
         }]
       };
 
-      let picker = await this.pickerController.create(options);
-      picker.present()
+      this.picker = await this.pickerController.create(options);
+      this.picker.present()
     }
 
+  }
+
+  ionViewWillLeave() {
+    if (this.picker) {
+      this.picker.dismiss();
+    }
   }
 
   getColumnOptions() {
