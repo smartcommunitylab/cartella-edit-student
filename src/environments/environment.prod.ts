7c65fccd305bf8c7e707596509730825a85da5c3
ffais <ffais@fbk.eu>
2021-09-15 12:12:20 +0200

    update config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,18 +3,19 @@ export const environment = {
   showTabs: true,
   appName: 'Cartella EDIT Studente',
   minimumAge: 14,
-  apiEndpoint: 'http://localhost:4050/cartella-asl/api',
- 
-  cordova_identity_client: '',
-  cordova_identity_server: '',
-  cordova_redirect_url: 'testappionicio://callback',
-  cordova_scopes: '',
-  cordova_end_session_redirect_url: '',
+  apiEndpoint: 'https://cartella-asl-demo.platform.smartcommunitylab.it/cartella-asl/api',
 
-  implicit_identity_client: '',
-  implicit_identity_server: '',
-  implicit_redirect_url: '',
-  implicit_scopes: '',
-  implicit_end_session_redirect_url: '',
+  cordova_identity_client: 'c_37b06a4d-9ca5-47bb-8630-283249916cdd',
+  cordova_identity_server: 'https://aac-adc.platform.smartcommunitylab.it/',
+  cordova_redirect_url: 'it.smartcommunitylab.editstudente.codescanner://callback',
+  cordova_scopes: 'openid profile email',
+  cordova_end_session_redirect_url: 'cartellaeditstudente://endSession',
+
+  implicit_identity_client: 'c_37b06a4d-9ca5-47bb-8630-283249916cdd',
+  implicit_identity_server: 'https://aac-adc.platform.smartcommunitylab.it/',
+  implicit_redirect_url: 'https://cartella-asl-demo.platform.smartcommunitylab.it/edit-studente/implicit/authcallback',
+  implicit_scopes: 'openid profile email',
+  implicit_end_session_redirect_url: 'https://cartella-asl-demo.platform.smartcommunitylab.it/edit-studente/implicit/endsession',
+
 
 };
