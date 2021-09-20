7ca0bc10432a550565286a3294de34d51eccd55c
micnori <micnori@yahoo.com>
2021-09-20 15:58:51 +0200

    env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,19 +3,20 @@ export const environment = {
   showTabs: true,
   appName: 'Cartella EDIT Studente',
   minimumAge: 14,
-  apiEndpoint: 'https://cartella-asl-demo.platform.smartcommunitylab.it/cartella-asl/api',
+	
+  apiEndpoint: 'https://cartella.provincia.tn.it/cartella-asl/api',
 
-  cordova_identity_client: 'c_37b06a4d-9ca5-47bb-8630-283249916cdd',
-  cordova_identity_server: 'https://aac-adc.platform.smartcommunitylab.it/',
+  cordova_identity_client: '7175611b-cb56-4d76-b516-ee2df4461711',
+  cordova_identity_server: 'https://aac.platform.smartcommunitylab.it/',
   cordova_redirect_url: 'it.smartcommunitylab.editstudente.codescanner://callback',
   cordova_scopes: 'openid profile email',
   cordova_end_session_redirect_url: 'cartellaeditstudente://endSession',
 
-  implicit_identity_client: 'c_37b06a4d-9ca5-47bb-8630-283249916cdd',
-  implicit_identity_server: 'https://aac-adc.platform.smartcommunitylab.it/',
-  implicit_redirect_url: 'https://cartella-asl-demo.platform.smartcommunitylab.it/edit-studente/implicit/authcallback',
+  implicit_identity_client: '7175611b-cb56-4d76-b516-ee2df4461711',
+  implicit_identity_server: 'https://aac.platform.smartcommunitylab.it',
+  implicit_redirect_url: 'https://cartella.provincia.tn.it/cartella/edit-studente/implicit/authcallback',
   implicit_scopes: 'openid profile email',
-  implicit_end_session_redirect_url: 'https://cartella-asl-demo.platform.smartcommunitylab.it/edit-studente/implicit/endsession',
+  implicit_end_session_redirect_url: 'https://cartella.provincia.tn.it/cartella/edit-studente/implicit/endsession',
 
 
 };
