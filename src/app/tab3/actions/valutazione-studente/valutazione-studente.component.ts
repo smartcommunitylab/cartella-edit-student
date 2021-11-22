428733796f4248ce9fdd68f4f92da5c77175e18c
Nawaz Khurshid <nawaz1981@gmail.com>
2021-11-22 14:46:49 +0100

    Fix for asl2-454

--- src/app/tab3/actions/valutazione-studente/valutazione-studente.component.ts
+++ src/app/tab3/actions/valutazione-studente/valutazione-studente.component.ts
@@ -28,40 +28,40 @@ export class ValutazioneStudenteComponent {
     private utilsService: UtilsService,
    ) { 
     
-    this.domande[0] = {
-      id: 1,
-      nome: 'Valutare le caratteristiche contestuali dell’ambiente per svolgere le proprie mansioni in maniera corretta e non fuori luogo ',
-      punteggio: '5',
-      rispostaChiusa: true
-    }
+    // this.domande[0] = {
+    //   id: 1,
+    //   nome: 'Valutare le caratteristiche contestuali dell’ambiente per svolgere le proprie mansioni in maniera corretta e non fuori luogo ',
+    //   punteggio: '5',
+    //   rispostaChiusa: true
+    // }
 
-    this.domande[1] = {
-      id: 2,
-      nome: 'Valutare le caratteristiche contestuali dell’ambiente per svolgere le proprie mansioni in maniera corretta e non fuori luogo',
-      punteggio: '4',
-      rispostaChiusa: true
-    }
+    // this.domande[1] = {
+    //   id: 2,
+    //   nome: 'Valutare le caratteristiche contestuali dell’ambiente per svolgere le proprie mansioni in maniera corretta e non fuori luogo',
+    //   punteggio: '4',
+    //   rispostaChiusa: true
+    // }
 
-    this.domande[2] = {
-      id: 3,
-      nome: 'Valutare le caratteristiche contestuali dell’ambiente per svolgere le proprie mansioni in maniera corretta e non fuori luogo',
-      punteggio: '3',
-      rispostaChiusa: true
-    }
+    // this.domande[2] = {
+    //   id: 3,
+    //   nome: 'Valutare le caratteristiche contestuali dell’ambiente per svolgere le proprie mansioni in maniera corretta e non fuori luogo',
+    //   punteggio: '3',
+    //   rispostaChiusa: true
+    // }
 
-    this.domande[3] = {
-      id: 4,
-      nome: 'Valutare le caratteristiche contestuali dell’ambiente per svolgere le proprie mansioni in maniera corretta e non fuori luogo. Valutare le caratteristiche contestuali dell’ambiente per svolgere le proprie mansioni in maniera corretta e non fuori luogo',
-      risposta: '2',
-      rispostaChiusa: false
-    }
+    // this.domande[3] = {
+    //   id: 4,
+    //   nome: 'Valutare le caratteristiche contestuali dell’ambiente per svolgere le proprie mansioni in maniera corretta e non fuori luogo. Valutare le caratteristiche contestuali dell’ambiente per svolgere le proprie mansioni in maniera corretta e non fuori luogo',
+    //   risposta: '2',
+    //   rispostaChiusa: false
+    // }
 
-    this.domande[4] = {
-      id: 5,
-      nome: 'Valutare le caratteristiche contestuali dell’ambiente per svolgere le proprie mansioni in maniera corretta e non fuori luogo',
-      punteggio: '0',
-      rispostaChiusa: true
-    }
+    // this.domande[4] = {
+    //   id: 5,
+    //   nome: 'Valutare le caratteristiche contestuali dell’ambiente per svolgere le proprie mansioni in maniera corretta e non fuori luogo',
+    //   punteggio: '0',
+    //   rispostaChiusa: true
+    // }
 
   }
 
@@ -86,6 +86,17 @@ export class ValutazioneStudenteComponent {
     this.router.navigate(['../../../'], { relativeTo: this.route });
   }
 
+  save() {
+    console.log('');
+    this.domande = this.prepareSaveArray(this.domande);
+  }
+
+  prepareSaveArray(arr) {
+    arr.forEach(element => {
+      delete element['ultimaModifica'];
+    });
+    return arr;
+  }
  
  
 }
