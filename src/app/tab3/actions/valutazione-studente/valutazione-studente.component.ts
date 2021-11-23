07812b9d2c349a6d8e0372a184458f74a27d5cb1
Nawaz Khurshid <nawaz1981@gmail.com>
2021-11-23 11:43:49 +0100

    Fix for asl2-454
    Fix for scroll event detection.

--- src/app/tab3/actions/valutazione-studente/valutazione-studente.component.ts
+++ src/app/tab3/actions/valutazione-studente/valutazione-studente.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { DataService } from '../../../core/services/data.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { UtilsService } from 'src/app/core/services/utils.service';
+import { ModalController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'valutazione-studente',
@@ -13,6 +15,7 @@ export class ValutazioneStudenteComponent {
   attivita;
   aa;
   es;
+  domande= []
   valutazione = [
     { titolo: 'Moltissimo', punteggio: 5 }, 
     { titolo: 'Molto', punteggio: 4 }, 
@@ -20,13 +23,19 @@ export class ValutazioneStudenteComponent {
     { titolo: 'Poco', punteggio: 2 }, 
     { titolo: 'Per nulla', punteggio: 1 }
   ];
-  domande;  
+  lastScrollTop = 130;
+  buttonShow:boolean;
+  domanteTotale = 0;
+  domandeCompilati = 0;
+  
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private dataService: DataService,
     private utilsService: UtilsService,
-   ) { 
+    private modalCtrl: ModalController,
+    private alertController: AlertController
+  ) { 
     
     // this.domande[0] = {
     //   id: 1,
@@ -73,7 +82,22 @@ export class ValutazioneStudenteComponent {
           this.attivita = attivita;
           this.aa = attivita.aa;
           this.es = attivita.es;
-
+          this.dataService.getValutazioneAttivita(this.es.id).subscribe((res) => {
+            this.domande = res.valutazioni;
+            this.domande.forEach(d=> {
+              if (d.rispostaChiusa) {
+                this.domanteTotale++;
+              }
+              if (d.punteggio > 0) {
+                this.domandeCompilati++;
+              }
+            })
+            this.utilsService.dismissLoading();
+          },
+          (err: any) => {
+            console.log(err);
+            this.utilsService.dismissLoading();
+          })
         },
           (err: any) => {
             console.log(err);
@@ -87,8 +111,10 @@ export class ValutazioneStudenteComponent {
   }
 
   save() {
-    console.log('');
     this.domande = this.prepareSaveArray(this.domande);
+    this.dataService.saveValutazioneEsperienza(this.domande, this.es.id).subscribe((res) => {
+      this.utilsService.presentSuccessLoading('Salvataggio effettuato con successo!');
+    });
   }
 
   prepareSaveArray(arr) {
@@ -97,6 +123,32 @@ export class ValutazioneStudenteComponent {
     });
     return arr;
   }
- 
- 
+
+  onScroll(event) {
+    this.buttonShow = this.lastScrollTop < event.detail.scrollTop;
+  };
+
+  checkLimit() {
+    var limit = 5;
+    const element = event.target as HTMLInputElement;
+    const value = element.value;
+    if (value.length <= limit) {
+      element.value = value;
+    } else {
+      this.utilsService.presentWarningLoading('Il limite per il numero massimo di caratteri è 250');
+      element.value = value.substr(0, limit-1);
+    }
+  }
+
+  styleReport() {
+    var style = {
+      'color': '#707070',
+      'font-size': '14px'
+    };
+    if (this.domandeCompilati == this.domanteTotale && this.domandeCompilati > 0) {
+      style['color'] = '#007A50'; // green
+    }
+    return style;
+  }
+
 }
